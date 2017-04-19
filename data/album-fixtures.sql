dfe0ac0f0162edfa3aa3b7b967a7bdbd70645c65
Ljubomir Reljin <reljin992@gmail.com>
2017-04-19 22:03:26 +0200

    Uradjen pagination

---
+++ data/album-fixtures.sql
@@ -0,0 +1,12 @@
+INSERT INTO 'album' ('artist', 'title')
+VALUES
+    ('David Bowie', 'The Next Day (Deluxe Version)'),
+    ('Bastille', 'Bad Blood'),
+    ('Bruno Mars', 'Unorthodox Jukebox'),
+    ('Emeli Sandé', 'Our Version of Events (Special Edition)'),
+    ('Bon Jovi', 'What About Now (Deluxe Version)'),
+    ('Justin Timberlake', 'The 20/20 Experience (Deluxe Version)'),
+    ('Bastille', 'Bad Blood (The Extended Cut)'),
+    ('P!nk', 'The Truth About Love'),
+    ('Sound City - Real to Reel', 'Sound City - Real to Reel'),
+    ('Jake Bugg', 'Jake Bugg');
